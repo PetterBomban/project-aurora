6deed64b9f46394349cf774b42d94e9c645810d9
Petter Bomban <petterbomban@gmail.com>
2017-01-18 14:12:13 +0100

    bg and debug draw

--- main.lua
+++ main.lua
@@ -5,15 +5,23 @@ function love.load()
     require('game/scripts/movement/movement')
     require('game/scripts/misc/keyboard')
     require('game/scripts/misc/mouse')
+
+    -- graphics
+    bg = love.graphics.newImage('game/assets/bg/space-4.jpg')
 end
 
 function love.draw()
+    --bg
+    love.graphics.draw(bg, 0, 0)
+
+    -- debug draws --
+    local fps = love.timer.getFPS()
+    love.graphics.print("fps: " .. tostring(fps), 0, 1037)
+
+    -- player
     Player:Draw()
 end
 
 function love.update(dt)
     Player:Update(dt)
-
-    wp = Movement:GetWaypoints(Player:GetPlayer())
-    print(Helper:DumpTable(wp))
 end
